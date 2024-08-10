ae54bee76eddb4ed892f5063aa0562e7079fb793
iqbalpa <iqbalpahlevi16@gmail.com>
2024-08-10 09:14:12 +0700

    feat: add cek posisi kelas

--- marineford.py
+++ marineford.py
@@ -1,4 +1,3 @@
-import time
 import requests
 import warnings
 from bs4 import BeautifulSoup
@@ -15,12 +14,15 @@ warnings.filterwarnings('ignore')
 AUTH_PAGE = "https://academic.ui.ac.id/main/Authentication/"
 HOME_PAGE = "https://academic.ui.ac.id/main/Welcome/Index"
 COURSE_PLAN_PAGE = "https://academic.ui.ac.id/main/CoursePlan/CoursePlanEdit"
+CEK_IRS_URL = 'https://academic.ui.ac.id/main/CoursePlan/CoursePlanViewCheck'
+IRS_DONE_PAGE = 'https://academic.ui.ac.id/main/CoursePlan/CoursePlanDone'
 SUBMIT_COURSE_PLAN_URL = "https://academic.ui.ac.id/main/CoursePlan/CoursePlanSave"
 
 # Configure loggers with specific colors
 auth_logger = configure_logger('auth', 'blue')
 course_plan_logger = configure_logger('course_plan', 'yellow')
 api_logger = configure_logger('api', 'green')
+cek_irs_logger = configure_logger('cek_irs', 'purple')
 
 # Helper Function
 def configure_driver():
@@ -104,12 +106,12 @@ def create_payload(matkul, token):
   return payload
 
 
-def create_headers():
+def create_headers(referer):
   headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
     'Content-Type': 'application/x-www-form-urlencoded',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Referer': COURSE_PLAN_PAGE,
+    'Referer': referer,
   }
   return headers
 
@@ -131,6 +133,28 @@ def get_submitted_kelas(content):
       else:
         submitted_kelas.add(nama_mk)
   return submitted_kelas
+
+
+def get_posisi_kelas(content):
+  soup = BeautifulSoup(content, 'html.parser')
+  rows = soup.find_all('tr')
+  results = []
+  for row in rows:
+    text = row.find('td').get_text(separator=' ', strip=True)
+    if '---' in text:
+      continue
+    if 'Kapasitas' in text or '; Kelas' in text:
+      results.append(text)
+  print('='*50)
+  i = 1
+  for res in results:
+    if 'Kapasitas' in res:
+      print(f'    {res}')
+    else:
+      print(f'[{i}] {res}')
+      i+=1
+  print('='*50)
+  return results
 
 
 def war():
@@ -171,7 +195,7 @@ def war():
 
   #========= 3. Send the Payload to the API
   payload = create_payload(matkul, token)
-  headers = create_headers()
+  headers = create_headers(COURSE_PLAN_PAGE)
   cookies = {cookie['name']: cookie['value'] for cookie in driver.get_cookies()}
 
   while True:
@@ -209,6 +233,37 @@ def war():
         api_logger.warning("Connection timed out. Retrying...")
       else:
         api_logger.error(f"An error occurred: {e}")
+
+  #========= 4. Cek Posisi Kelas
+  cek_irs_logger.info("Accessing Pengecekan IRS...")
+  headers = create_headers(IRS_DONE_PAGE)
+  cookies = {cookie['name']: cookie['value'] for cookie in driver.get_cookies()}
+
+  while True:
+    try:
+      response = requests.post(
+        CEK_IRS_URL,
+        headers=headers,
+        cookies=cookies,
+        verify=False
+      )
+      cek_irs_logger.info(f"Response Status Code: {response.status_code}")
+
+      if response.status_code == 200:
+        cek_irs_logger.info("Posisi Kelas!")
+        get_posisi_kelas(response.text)
+        break
+
+      else:
+        cek_irs_logger.error(f"Failed to check the IRS. Status code: {response.status_code}")
+        cek_irs_logger.error(f"Response content: {response.text}")
+        raise Exception("Non-200 status code received")
+
+    except Exception as e:
+      if isinstance(e, requests.exceptions.ConnectTimeout):
+        cek_irs_logger.warning("Connection timed out. Retrying...")
+      else:
+        cek_irs_logger.error(f"An error occurred: {e}")
 
   driver.quit()
 
